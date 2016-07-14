137600ff80665d13b6fde28acf74cdbc99584f54
r_219 <rinathaisman@gmail.com>
2016-07-15 01:27:53 +0300

    Загружаю проект

---
+++ demo/js/demo.js
@@ -0,0 +1,26 @@
+/*
+
+EXPAND COLLAPSE
+
+*/
+
+ $(document).ready(function(){
+ $('#toggle-switcher').click(function(){
+		if($(this).hasClass('open')){
+			$(this).removeClass('open');
+			$('#switch-style').animate({'left':'-220px'});
+		}else{
+			$(this).addClass('open');
+			$('#switch-style').animate({'left':'0'});
+		}
+	});
+});
+
+/* ==============================================
+Home Super Slider (images)
+=============================================== */
+
+ $('#titlebar').superslides({
+      animation: 'fade',
+    });
+	
